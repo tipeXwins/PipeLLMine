34b3a6d213e6b80c225d4f9fbef23d722991fbd3
tipeXwins <victorymuchi2013@gmail.com>
2023-05-22 20:18:22 +0100

    plbart query creation for inserting special tokens

--- Tests/Query_test.py
+++ Tests/Query_test.py
@@ -1,15 +1,14 @@
 import unittest
 #from Pipeline.Query_Creation import *
 from Pipeline.use import readFile
+from Pipeline.Query_Creation import HFHintQuery, HFPlBartStandardQuery, HFStandardQuery, OAIHintQuery
 
 class QueryTestCase(unittest.TestCase):
     def test_HFHintQuery(self):
-        queryGenerator = HFHintQuery()
-        f = open('./resources/input.py', "r")
+        queryGenerator = HFHintQuery("<mask>",[3],None,[3])
+        f = open('Tests/resources/input.py', "r")
         text = f.readlines()
         f.close()
-        queryGenerator.setPlaceholder("<mask>")
-        queryGenerator.setLinesAddHint([3])
         resultQueryGenerated = queryGenerator.createQuery(text)
         print("Query", resultQueryGenerated)
 
@@ -20,7 +19,7 @@ class QueryTestCase(unittest.TestCase):
         self.assertEqual(True, False)  # add assertion here
 
     def test_HFStandardQuery(self):
-        f = open('./resources/input.py', "r")
+        f = open('Tests/resources/input.py', "r")
         text = f.readlines()
         f.close()
         queryGenerator = HFStandardQuery()
@@ -33,14 +32,27 @@ class QueryTestCase(unittest.TestCase):
         queryExpected = "<TODO>" ## TODO: here to write the query that we expect
 
         self.assertEqual(queryExpected, resultQueryGenerated)
+    
+    def test_HFPlBartStandardQuery(self):
+        f = open('Tests/resources/input.py', "r")
+        text = f.readlines()
+        f.close()
+        queryGenerator = HFPlBartStandardQuery("<mask>",[4])
+
+        resultQueryGenerated = queryGenerator.createQuery(text)
+        print("Query", resultQueryGenerated)
+
+        queryExpected = ['<s> def bitcount(n):\n', '    count = 0\n', '    while n:\n', '', '    <mask>', '    return count\n', '</s> Python'] 
+
+        self.assertEqual(queryExpected, resultQueryGenerated)
 
     def test_OpenAIQuery(self):
         print("Init test")
         hola = OAIHintQuery()
         hola.setLinesAddHint([2, 3])
 
-        file = readFile('../Codes/QuixBugs/BuggyCodes/', 'bitcount.py')
-        queryGenerated = hola.createQuery(file)
+        #file = readFile('../Codes/QuixBugs/BuggyCodes/', 'bitcount.py')
+        #queryGenerated = hola.createQuery(file)
 
     def test_init(self):
         print("init")
