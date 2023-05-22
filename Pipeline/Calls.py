39b899a3233d4238849a6a70b28098c976986a04
Matias Martinez <matomartinez@gmail.com>
2023-05-22 19:50:50 +0200

    testing codeGen 350M

--- Pipeline/Calls.py
+++ Pipeline/Calls.py
@@ -2,6 +2,8 @@ import os
 import openai
 import torch
 from transformers import pipeline , AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
+from enum import Enum
+
 
 class OAICommunicationController():
     openai.api_key = "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"
@@ -46,7 +48,7 @@ class OAICommunicationController():
         return response['choices'][0]['text']
     
 
-    
+
 class HFCommunicationController():
     def callToModelWithTransformers(self,query):
         print("THIS IS AN ABSTRACT CLASS PLEASE REFFER TO AN SPECIFIC Hugging Face Controller")
@@ -72,7 +74,8 @@ class HFPlBartController(HFCommunicationController):
             output.append(aSolution)
 
         return output[0]
-    
+
+
 class HFCodeT5Controller(HFCommunicationController):
     MAX_LENGTH = 512
     NUM_BEAMS = 10
@@ -87,14 +90,24 @@ class HFCodeT5Controller(HFCommunicationController):
         for generated_id in generated_ids:
             output.append(self.tokenizer.decode(generated_id, skip_special_tokens=True))
         return output
-    
+class CodeGenSize(Enum):
+    size350M =""
+    size2B =""
+    size6B =""
+    size16B =""
+
+class CodeGenData(Enum):
+    nl = "nl"
+    multi = "multi"
+    mono = "mono"
+
 class HFCodeGenController(HFCommunicationController):
     MAX_NEW_TOKENS = 128
     NUM_BEAMS = 10
     NUM_RETURN_SEQUENCES = 10
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-2B-mono")
-        self.model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-2B-mono")
+    def __init__(self, modelIdentifier = "350M", data="mono"):
+        self.tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-{}-{}".format(modelIdentifier, data))
+        self.model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-{}-{}".format(modelIdentifier, data))
     def callToModelWithTransformers(self,query):
         input_ids = self.tokenizer(query, return_tensors="pt").input_ids
         eos_id = self.tokenizer.convert_tokens_to_ids(self.tokenizer.eos_token)
@@ -105,7 +118,7 @@ class HFCodeGenController(HFCommunicationController):
         output = []
         for generated_id in generated_ids:
             output.append(self.tokenizer.decode(generated_id, skip_special_tokens=True))
-        return output[0]
+        return output
 
 class HFIncoderController(HFCommunicationController):
     MAX_NEW_TOKENS = 128
