42a357ec65bc3cb36be9eb0b8ce06b41b6b8b8ad
tipeXwins <victorymuchi2013@gmail.com>
2023-06-19 13:00:33 +0200

    Folder Cleaning

--- Tests/resources/inputs/codet5Query.py
+++
@@ -1,6 +0,0 @@
-def bitcount(n):
-    count = 0
-    while n:
-        <extra_id_0>
-        count += 1
-    return count
--- Tests/resources/inputs/plbartQuery.py
+++
@@ -1,7 +0,0 @@
-def bitcount(n):
-    count = 0
-    while n:
-        #buggy line: n ^= n - 1
-        <mask>  
-        count += 1
-    return count
